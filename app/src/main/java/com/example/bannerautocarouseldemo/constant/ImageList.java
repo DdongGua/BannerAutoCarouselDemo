6c2598e21508a95ce15b043ba1ab817838a29b72
亮亮 <1694767775@qq.com>
2018-06-25 19:45:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bannerautocarouseldemo/constant/ImageList.java
@@ -0,0 +1,24 @@
+package com.example.bannerautocarouseldemo.constant;
+
+import com.example.bannerautocarouseldemo.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 亮亮 on 2018/6/25.
+ */
+
+public class ImageList {
+    public static ArrayList<Integer> getDefault() {
+        ArrayList<Integer> imageList = new ArrayList<Integer>();
+        imageList.add(R.mipmap.banner_1);
+        imageList.add(R.mipmap.banner_2);
+        imageList.add(R.mipmap.banner_3);
+        imageList.add(R.mipmap.banner_4);
+        imageList.add(R.mipmap.banner_5);
+        imageList.add(R.mipmap.a);
+        imageList.add(R.mipmap.b);
+        imageList.add(R.mipmap.c);
+        return imageList;
+    }
+}
